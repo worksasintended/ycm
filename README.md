49b2f0ebdb425593efa1c55f95e0e2438ca0d5aa
Marc <marc@marschalls.net>
2019-03-11 09:12:28 +0100

    added install script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ycm
 ycm snippets and configs
+Make sure to pull recoursivly via 
+```
+git clone --recurse-submodules -j8 https://github.com/worksasintended/ycm
+```
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Make sure you cloned the repo recoursivly with sub repos, otherwise this will not work."
+echo -e "Default \e[31m THIS WILL OVERWRITE YOUR VIMRC AND VIMCONFIG!!!\e[0m"  
+read -p "Press enter to continue.... "
+cp -rf .vim* ~/
